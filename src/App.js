c696cb56fb609b427712b177d2aef0a0fc2eff51
Julien <gerard.julien2000@orange.fr>
2022-02-09 16:24:20 +0100

    add items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import List from './List'
+import Alert from './Alert'
 function App() {
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!name) {
+      //alert
+    }
+    else if (name && isEditing) {
+      //deal with edit
+    }
+    else {
+      //show alert
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setList([...list, newItem]);
+      setName('')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // il faut centrer la div
+    <div className="container">
+      <form className="" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Shopping List</h3>
+        <div className="submit">
+          <input type="text" placeholder="Enter a Product" value={name} onChange={(e) => setName(e.target.value)} />
+          <button type="submit" className="btn" ></button>
+          {isEditing ? 'edit' : 'submit'}
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="">
+          <List items={list} />
+          <button className="btn">Clear Items</button>
+        </div>
+
+      )}
+
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaEdit, FaTrash } from 'react-icons/fa'
+
+function List({ items }) {
+    return (
+        <div>
+            {items.map((item) => {
+                const { id, title } = item;
+                return (
+                    <div key={id} className="">
+                        <p className="">{title}</p>
+                        <div className="btn-group">
+                            <button type='button' className="btn"><FaEdit/></button>
+                            <button type='button' className="btn"><FaTrash/></button>
+
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
